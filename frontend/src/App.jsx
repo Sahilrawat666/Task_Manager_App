9113c0989785c318898779ac6cc6369a87ff9bbd
Sahilrawat666 <sr4328914@gmail.com>
2026-03-21 13:02:16 +0530

    initial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./Components/custom/Navbar";
+import Navbar from "./components/custom/Navbar";
 import { Route, Router, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
