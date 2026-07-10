b830b50e3644b50e938f419e0e58641da56accd7
Sahilrawat666 <sr4328914@gmail.com>
2026-07-10 13:06:02 +0530

    .

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,13 +1,18 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
+
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
-    family: 4, // Force IPv4
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+        rejectUnauthorized: false,
     },
 });
 
